3c032a079e47505b36767df88ffe6abc43bbbe9d
Geovany Fuentes <geofuentes.gf@gmail.com>
2019-07-13 23:46:09 -0600

    Nueva base de datos

--- medicaldriver.sql
+++ medicaldriver.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-07-2019 a las 03:48:23
+-- Tiempo de generación: 14-07-2019 a las 07:45:42
 -- Versión del servidor: 10.3.16-MariaDB
 -- Versión de PHP: 7.3.7
 
